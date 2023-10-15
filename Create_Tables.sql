12b57e905003f566a061cbef76fbb717643626c6
Duddu-Hriday <210010016@iitdh.ac.in>
2023-10-15 23:51:04 +0530

    Altered Create_Tables.sql

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE BANK (
 			
-			branch_id	CHAR(3) NOT NULL,
-			bank_id		CHAR(3) NOT NULL,
+			branch_id	VARCHAR(100) NOT NULL,
+			bank_id		VARCHAR(100) NOT NULL,
 			branch		VARCHAR(255) NOT NULL,
 			bank_name	VARCHAR(255) NOT NULL,
 			
@@ -11,44 +11,45 @@ CREATE TABLE BANK (
 			);
 			
 CREATE TABLE CUSTOMER (
-			
-			cust_id CHAR(5) NOT NULL PRIMARY KEY,
-			name	VARCHAR(100) NOT NULL,
-			phone	NUMERIC(10,0) NOT NULL,
-			email	VARCHAR(100),
-			mpin	NUMERIC(6,0) NOT NULL,
-			acc_no	CHAR(11) NOT NULL,
-			balance	NUMERIC(15,0) CHECK(balance >= 0),
-			bank_id VARCHAR(255) NOT NULL REFERENCES BANK(bank_id)
-			
-			);
+    cust_id VARCHAR(100) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    mpin VARCHAR(100) NOT NULL,
+    acc_no VARCHAR(100) NOT NULL,
+    balance INT CHECK(balance >= 0),
+    bank_id VARCHAR(255) NOT NULL ,
+    branch_id VARCHAR(100) NOT NULL,
+	FOREIGN KEY (bank_id, branch_id) REFERENCES BANK (bank_id, branch_id)
+);
+
 			
 CREATE TABLE TRANSACTIONS (
 
-			trans_id	CHAR(22) NOT NULL,
-			sender_id	CHAR(5) NOT NULL REFERENCES CUSTOMER(cust_id),
-			reciever_id	CHAR(5) NOT NULL REFERENCES CUSTOMER(cust_id),
+			trans_id	VARCHAR(100) NOT NULL,
+			sender_id	VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
+			reciever_id	VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
 			dte			date,
 			time		timestamp,
-			amount		NUMERIC(15,0) NOT NULL
+			amount		INT NOT NULL
 			
 			);
 			
 CREATE TABLE LOAN (
 
-			loan_id		CHAR(8) NOT NULL PRIMARY KEY,
+			loan_id		VARCHAR(100) NOT NULL PRIMARY KEY,
 			bank_id		VARCHAR(255) NOT NULL REFERENCES BANK(bank_id),
-			cust_id		CHAR(5) NOT NULL REFERENCES CUSTOMER(cust_id),
-			amount		NUMERIC(15,0) NOT NULL,
-			emi			NUMERIC(15,0) NOT NULL,
+			cust_id		VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
+			amount		int  NOT NULL,
+			emi			int NOT NULL,
 			deadline	timestamp
 
 			);
 			
 CREATE TABLE DEFAULTER (
 
-			cust_id CHAR(5) NOT NULL REFERENCES CUSTOMER(cust_id),
-			due 	NUMERIC(15,0) NOT NULL,
-			penalty NUMERIC(15,0) NOT NULL
+			cust_id VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
+			due 	int  NOT NULL,
+			penalty int NOT NULL
 
 			);
