3cf45600ea758e26f60b2e9e3c83de0b659c2439
Duddu-Hriday <210010016@iitdh.ac.in>
2023-10-17 17:51:10 +0530

    updated customer.py

--- customer.py
+++ customer.py
@@ -53,6 +53,14 @@ def email_validate(email):
         return True
     return False
 
+def cust_id_generator(cust_id):
+    if max_id is None:
+        Cust_id = "00000"
+    int_id = int(cust_id)+1
+    formatted_string = f'{int_id:05}'
+    return formatted_string
+
+#########################################################################################################
 conn= psycopg2.connect(
     host="localhost",
     port=5432,
@@ -84,6 +92,7 @@ print("\n\n")
 
 print("     1.New Customer: Sign Up                  2.Existing Customer: Login\n")
 choice =input("Please Enter your choice: ")
+############################################################################################################
 if choice == "1":
     print("\n\n             Please Fill the following Details:\n")
     Name=input("Enter Your Name: ")
@@ -126,15 +135,17 @@ if choice == "1":
         count = count + 1
 
 ###########################################################################################################
-
-    sql = "SELECT branch FROM bank where bank_id = Bank_id;"
-    mycursor.execute(sql)
+    print(Bank_id)
+    print("             Branches Available \n")
+    sql = "SELECT branch FROM bank where bank_id = %s;"
+    mycursor.execute(sql,(Bank_id,))
     rows = mycursor.fetchall()
     count = 1
     for row in rows:
         row_str = '\t'.join(map(str, row))
         print(count,". ",row_str)
         count = count + 1
+    
 
 ###########################################################################################################
 
@@ -151,8 +162,10 @@ if choice == "1":
         count = count + 1
 
 ###########################################################################################################
-
-    Cust_id  =  "2"
+    sql = "SELECT max(cust_id) FROM customer;"
+    mycursor.execute(sql)
+    max_id = mycursor.fetchone()[0]
+    Cust_id=cust_id_generator(max_id)
     Account_num=account_number(Bank_id,Branch_id,Cust_id)
     Customer_1 = Customer(Cust_id,Name,Phone,Email,M_pin,0,Bank_id,Branch_id,Account_num)
     # sql = "INSERT INTO customer values ('Cust_id','Name','Phone','Email','M_pin','Account_num',0,'Bank_id','Branch_id');"
@@ -163,7 +176,72 @@ if choice == "1":
 
     # cust_id | name | phone | email | mpin | acc_no | balance | bank_id | branch_id
     mycursor.execute(sql)
-    conn.commit()
+    print("\n\nSign in Successful.. Thank you for choosing phonepe. What would you like to do today: ")
+###########################################################################################################
+    
+elif choice == "2":
+    print("\n\nPlease Enter your details as follows:\n")
+    Cust_id = input("Enter your customer id: ")
+    UPI_Pin = input("Enter your UPI pin: ")
+
+    # Check if the customer with the given Cust_id and UPI_Pin exists in the database
+    sql = "SELECT * FROM customer WHERE cust_id = %s AND mpin = %s;"
+    mycursor.execute(sql, (Cust_id, UPI_Pin))
+    data = mycursor.fetchone()
+    # Customer_1 = Customer(Cust_id,Name,Phone,Email,M_pin,0,Bank_id,Branch_id,Account_num)
+    
+
+    if data is not None:
+        Customer_1 = Customer(data[0],data[1],data[2],data[3],data[4],data[6],data[7],data[8],data[5])
+        # Customer authentication is successful
+        print("\nLogin successful. Welcome back, " + Customer_1.Name + "!")  # Assuming the name is in the second column (index 1)
+        # Now you can implement the functionality for existing customers
+        # For example, display account balance, perform transactions, etc.
+
+        # Implement your functionality here
+
+    else:
+        print("\nLogin failed. Please check your customer id and UPI pin.")
+
+################################################################################################################
+
+print("\n\n         What would you like to do today:\n")
+print("         1.Make a Transaction                     2.Check Your Balance\n")
+print("         3.Check Your Transactions list           4. Take a loan\n")
+print("         5.Check your details                     6. Edit your details\n")
+choice_today = input("Enter your choice: ")
+#############################################################################################################
+if choice_today=="5":
+    pin = input("Enter the upi pin: ")
+    if pin == Customer_1.M_pin:
+        print("\n\n----------------------------------------------------------------")
+        print("\n         Customer id: ",Customer_1.Cust_id,"\n")
+        print("         Name: ",Customer_1.Name,"\n")
+        print("         Phone Number: ",Customer_1.Phone,"\n")
+        print("         Email: ",Customer_1.Email,"\n")
+        print("         Account Number: ",Customer_1.Account_num,"\n")
+        print("         Balance: Rs.",Customer_1.Balance,"\n")
+        print("----------------------------------------------------------------")
+    else:
+        print("Invalid UPI Pin!!!!")
+
+#############################################################################################################
+# if choice == "1":
+#     sql ="begin;"
+#     mycursor.execute(sql)
+#     acc_num = input("Enter the customer id to which you would like to transfer money: ")
+#     pin = input("Enter your upi pin: ")
+#     if pin == Customer_1.M_pin:
+#         amount = int(input("Enter the amount you would like to transfer: "))
+#         balance_amount = int(Customer_1.Balance) - amount
+#         if balance_amount > 0:
+
+
+
+conn.commit()
+conn.close()
+mycursor.close()
+
 
 
 
