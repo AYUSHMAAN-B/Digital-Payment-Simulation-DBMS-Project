f5d02085415a2f370c81a1894af213a65f26e0f1
AYUSHMAAN-B <ayushmaanbn@gmail.com>
2023-10-21 09:10:52 +0530

    edited customer.py

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -11,22 +11,25 @@ CREATE TABLE BANK (
 			);
 			
 CREATE TABLE CUSTOMER (
-    cust_id VARCHAR(100) NOT NULL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    phone VARCHAR(100) NOT NULL,
-    email VARCHAR(100),
-    mpin VARCHAR(100) NOT NULL,
-    acc_no VARCHAR(100) NOT NULL,
-    balance INT CHECK(balance >= 0),
-    bank_id VARCHAR(255) NOT NULL ,
-    branch_id VARCHAR(100) NOT NULL,
-	FOREIGN KEY (bank_id, branch_id) REFERENCES BANK (bank_id, branch_id)
+
+			cust_id 	VARCHAR(100) NOT NULL PRIMARY KEY,
+			name 		VARCHAR(100) NOT NULL,
+			phone 		VARCHAR(100) NOT NULL,
+			email 		VARCHAR(100),
+			mpin 		VARCHAR(100) NOT NULL,
+			acc_no 		VARCHAR(100) NOT NULL,
+			balance 	INT CHECK(balance >= 0),
+			bank_id 	VARCHAR(255) NOT NULL ,
+			branch_id 	VARCHAR(100) NOT NULL,
+
+			FOREIGN KEY (bank_id, branch_id) REFERENCES BANK (bank_id, branch_id)
+
 );
 
 			
 CREATE TABLE TRANSACTIONS (
 
-			trans_id	VARCHAR(100) NOT NULL,
+			trans_id	VARCHAR(100) NOT NULL PRIMARY KEY,
 			sender_id	VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
 			reciever_id	VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
 			dte			VARCHAR(100),
@@ -40,16 +43,16 @@ CREATE TABLE LOAN (
 			loan_id		VARCHAR(100) NOT NULL PRIMARY KEY,
 			bank_id		VARCHAR(255) NOT NULL REFERENCES BANK(bank_id),
 			cust_id		VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
-			amount		int  NOT NULL,
-			emi			int NOT NULL,
+			amount		INT  NOT NULL,
+			emi			INT NOT NULL,
 			deadline	timestamp
 
 			);
 			
 CREATE TABLE DEFAULTER (
 
-			cust_id VARCHAR(100) NOT NULL REFERENCES CUSTOMER(cust_id),
-			due 	int  NOT NULL,
-			penalty int NOT NULL
+			cust_id 	VARCHAR(100) NOT NULL REFERENCES LOAN(cust_id),
+			due 		INT  NOT NULL,
+			penalty 	INT NOT NULL
 
 			);
--- customer.py
+++ customer.py
@@ -1,7 +1,8 @@
 import psycopg2
 import datetime
+
 class Customer:
-    def __init__(self, Cust_id,Name,Phone,Email,M_pin,Balance,Bank_id,Branch_id,Account_num):
+    def __init__(self, Cust_id, Name, Phone, Email, M_pin, Balance, Bank_id, Branch_id, Account_num):
         self.Cust_id   =  Cust_id
         self.Name      =  Name
         self.Phone     =  Phone
@@ -13,50 +14,58 @@ class Customer:
         self.Account_num = Account_num
 
 class Transactions:
-    def __init__(self,Transaction_id,Receive_id,Send_id,amount):
+    def __init__(self, Transaction_id, Receive_id, Send_id, amount):
         self.Transaction_id =   Transaction_id
         self.Receive_id     =   Receive_id
         self.Send_id        =   Send_id
         self.amount         =   amount
 
 class Loan:
-    def __init__(self,Loan_id,Amount,Deadline,EMI):
+    def __init__(self, Loan_id, Amount, Deadline, EMI):
         self.Loan_id  =     Loan_id
         self.Amount   =     Amount
         self.Deadline =     Deadline
         self.EMI      =     EMI
 
 class Defaulters:
-    def __intit__(self,Penality,Due):
+    def __intit__(self, Penality, Due):
         self.Penality   =     Penality
         self.Due        =     Due
+
 ###########################################################################################################
-def account_number(bank_id,branch_id,cust_id):
+
+def account_number(bank_id, branch_id, cust_id):
     return bank_id+branch_id+cust_id
 
 def phone_num_validate(num):
     if(len(num)==10):
         return True
+    
     return False
 
 def mpin_validate(num):
     if(len(num)==6):
         return True
+    
     return False
 
 def email_validate(email):
     if(len(email)<10):
         return False
+    
     email_val = email[-10:]
     if(email_val=="@gmail.com"):
         return True
+    
     return False
 
 def cust_id_generator(cust_id):
     if max_id is None:
         Cust_id = "00000"
+
     int_id = int(cust_id)+1
     formatted_string = f'{int_id:05}'
+
     return formatted_string
 
 def transaction_id_generator(sender,reciever):
@@ -68,7 +77,9 @@ def transaction_id_generator(sender,reciever):
     current_minute = current_datetime.minute
     current_second = current_datetime.second
     return sender+str(current_hour)+str(current_minute)+str(current_second)+str(current_day)+str(current_month)+str(current_year)+reciever
+
 #########################################################################################################
+
 conn= psycopg2.connect(
     host="localhost",
     port=5432,
@@ -87,7 +98,7 @@ styled_text = "\033[1;95m" + text + "\033[0m"
 print ("|--------------------------------------------------------------------------------------|")
 print ("|                                                                                      |")
 print ("|                                "+styled_text+"                                               |")
-print ("|                A trustworth app for your transactions,loans, bills and many more     |")
+print ("|                A trustworthy app for your transactions, loans, bills and many more   |")
 print ("|                                                                                      |")
 print ("|--------------------------------------------------------------------------------------|")
 
@@ -108,12 +119,14 @@ if choice == "1":
     Phone = input("Enter Your Phone Number: ")
     while(phone_num_validate(Phone)==False):
         Phone = input("Invalid phone number. Please Enter Again: ")
+
     Email = input("Enter Your Email id: ")
     while(email_validate(Email)==False):
         Email = input("Invalid Email entered. Email should end with '@gmail.com'.\nPlease Enter Again: ")
+
     M_pin = input("Enter a 6-digit UPI pin: ")
     while(mpin_validate(M_pin)==False):
-        M_pin = input("UPI Pin should be 6 digits.Please Enter Again: ")
+        M_pin = input("UPI Pin should be 6 digits. Please Enter Again: ")
 
 
 ###########################################################################################################
@@ -123,11 +136,13 @@ if choice == "1":
     mycursor.execute(sql)
     rows = mycursor.fetchall()
     count = 1
+
     for row in rows:
         row_str = '\t'.join(map(str, row))
         print(count,". ",row_str)
         count = count +1
-    ch = int(input("Enter in which bank is your account: "))
+
+    ch = int(input("Enter in which bank is your account in: "))
 
 ###########################################################################################################
 
@@ -135,11 +150,14 @@ if choice == "1":
     mycursor.execute(sql)
     rows = mycursor.fetchall()
     count = 1
+
     for row in rows:
         row_str = '\t'.join(map(str, row))
+
         if count == ch:
             Bank_id = row_str
             break
+
         count = count + 1
 
 ###########################################################################################################
@@ -149,12 +167,12 @@ if choice == "1":
     mycursor.execute(sql,(Bank_id,))
     rows = mycursor.fetchall()
     count = 1
+
     for row in rows:
         row_str = '\t'.join(map(str, row))
         print(count,". ",row_str)
         count = count + 1
     
-
 ###########################################################################################################
 
     ch=int(input("Enter the branch of the bank in which your account is present: "))
@@ -162,6 +180,7 @@ if choice == "1":
     mycursor.execute(sql,(Bank_id,))
     rows = mycursor.fetchall()
     count  = 1
+
     for row in rows:
         row_str = '\t'.join(map(str, row))
         if count == ch:
@@ -170,13 +189,14 @@ if choice == "1":
         count = count + 1
 
 ###########################################################################################################
+
     sql = "SELECT max(cust_id) FROM customer;"
     mycursor.execute(sql)
     max_id = mycursor.fetchone()[0]
     Cust_id=cust_id_generator(max_id)
-    Account_num=account_number(Bank_id,Branch_id,Cust_id)
-    Customer_1 = Customer(Cust_id,Name,Phone,Email,M_pin,0,Bank_id,Branch_id,Account_num)
-    # sql = "INSERT INTO customer values ('Cust_id','Name','Phone','Email','M_pin','Account_num',0,'Bank_id','Branch_id');"
+    Account_num=account_number(Bank_id, Branch_id, Cust_id)
+    Customer_1 = Customer(Cust_id, Name, Phone, Email, M_pin, 0, Bank_id, Branch_id, Account_num)
+    
     sql = "INSERT INTO customer (cust_id, name, phone, email, mpin, acc_no, balance, bank_id, branch_id) " \
       f"VALUES ('{Customer_1.Cust_id}', '{Customer_1.Name}', '{Customer_1.Phone}', " \
       f"'{Customer_1.Email}', '{Customer_1.M_pin}', '{Customer_1.Account_num}', " \
